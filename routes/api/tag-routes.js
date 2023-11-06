afea704b205d5809bfc187d206177d25a6841b1f
clayg <claytondguerrero@gmail.com>
2023-11-05 21:23:50 -0600

    completed get Tag by id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -7,19 +7,26 @@ const { findAll } = require("../../models/Product");
 router.get("/", async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-  // try {
+  try {
     const tagData = await Tag.findAll({
-      include: [{model: Product}]
-    })
-    res.status(200).json(tagData)
-  // } catch (error) {
-  //   res.status(500).json({message: 'No Tag Data Avalible'})
-  // }
+      include: [{ model: Product }],
+    });
+    res.status(200).json(tagData);
+  } catch (error) {
+    res.status(500).json({ message: "No Tag Data Avalible" });
+  }
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!tagData) res.status(404).json({ message: "404 : Tag Not Found" });
+    else res.status(200).json(tagData);
+  } catch (error) {}
 });
 
 router.post("/", (req, res) => {
