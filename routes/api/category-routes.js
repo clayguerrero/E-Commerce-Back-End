87aa42b7dce57508bda688f71bf568712c6d7122
clayg <claytondguerrero@gmail.com>
2023-11-06 17:16:42 -0600

    completed Category create

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,20 +7,38 @@ router.get("/", async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
-    const categories = await Category.findAll({ include: [{ model: Product }] })
-    res.status(200).json(categories)
+    const categories = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categories);
   } catch (error) {
-    res.status(500).json({message : 'No Category Data'})
+    res.status(500).json({ message: "No Category Data" });
   }
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!category)
+      res.status(404).json({ message: "404 : Category Not Found" });
+    else res.status(200).json(category);
+  } catch (error) {
+    res.status(500).json({ message: "No Category Data" });
+  }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (error) {
+    res.status(400).json({ message: "WARNING : Could Not Create Category" });
+  }
 });
 
 router.put("/:id", (req, res) => {
