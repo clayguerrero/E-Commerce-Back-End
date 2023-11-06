a35555d524fbe97e75332246a979a48bda83332d
clayg <claytondguerrero@gmail.com>
2023-11-06 17:02:49 -0600

    completed update tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -32,15 +32,26 @@ router.get("/:id", async (req, res) => {
 router.post("/", async (req, res) => {
   // create a new tag
   try {
-    const tagData = await Tag.create(req.body)
-    res.status(200).json(tagData)
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
   } catch (error) {
-    res.status(400).json({message: 'WARNING : Could Not Create Tag'})
+    res.status(400).json({ message: "WARNING : Could Not Create Tag" });
   }
 });
 
 router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    }).then((updated) => {
+      res.status(200).json(updated);
+    });
+  } catch (error) {
+    res.status(400).json({message : 'WARNING : COULD NOT UPDATE TAG'});
+  }
 });
 
 router.delete("/:id", async (req, res) => {
@@ -48,13 +59,13 @@ router.delete("/:id", async (req, res) => {
   try {
     const tagData = await Tag.destroy({
       where: {
-        id: req.params.id
-      }
-    })
-    if (!tagData) res.status(404).json({ message: "404 : Tag Not Found" })
-    else res.status(500).json(tagData)
+        id: req.params.id,
+      },
+    });
+    if (!tagData) res.status(404).json({ message: "404 : Tag Not Found" });
+    else res.status(500).json(tagData);
   } catch (error) {
-    res.status(500).json({message: 'WARNING : Tag Could Not Be Deleted!'})
+    res.status(500).json({ message: "WARNING : Tag Could Not Be Deleted!" });
   }
 });
 
