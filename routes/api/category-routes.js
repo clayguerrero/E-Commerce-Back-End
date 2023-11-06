dd05cc29637866325123084d3ce56b1875b0fb34
clayg <claytondguerrero@gmail.com>
2023-11-06 17:05:52 -0600

    completed find all categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,10 +3,15 @@ const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  
+  try {
+    const categories = await Category.findAll({ include: [{ model: Product }] })
+    res.status(200).json(categories)
+  } catch (error) {
+    res.status(500).json({message : 'No Category Data'})
+  }
 });
 
 router.get("/:id", (req, res) => {
